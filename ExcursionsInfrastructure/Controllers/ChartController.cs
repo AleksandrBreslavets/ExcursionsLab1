7bb9707ebc1740893c2cb609c2c07f02470ed5b2
AleksandrBreslavets <sasha.bresl2612005@gmail.com>
2024-03-04 00:39:02 +0200

    Етап 1.5 виконаний

---
+++ ExcursionsInfrastructure/Controllers/ChartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExcursionsInfrastructure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly ExcursionsDbContext _context;
+        public ChartController(ExcursionsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonData")]
+        public JsonResult JsonData()
+        {
+            var excursions = _context.Excursions.Include(e => e.Visitors).ToList();
+            List<object> excurVisitor= new List<object>();
+            excurVisitor.Add(new[] { "Екскурсія", "Кількість відвідувачів" });
+            foreach(var e in excursions)
+            {
+                excurVisitor.Add(new object[] { e.Name, e.Visitors.Count() });
+            }
+
+            return new JsonResult(excurVisitor);
+        }
+    }
+}
